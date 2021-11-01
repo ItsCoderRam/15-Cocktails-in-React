b9cd8539705f2cd25f3210590109bfdadb406596
ClimaxCoder <[ramnathcse123@gmail.com]>
2021-11-01 10:47:01 +0530

    Cocktails in React

---
+++ 15-Cocktails/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos explicabo odit repudiandae quasi sapiente. Recusandae quod corporis necessitatibus harum! Commodi natus officiis, nulla praesentium beatae voluptas nesciunt in, eos quo sunt non nam ea possimus reiciendis quisquam expedita optio enim?
+      </p>
+    </section>
+  )
+}
+
+export default About
